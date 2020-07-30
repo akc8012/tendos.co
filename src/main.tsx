96735375d67e09dfea2ddc5f024d1f85a476b682
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-30 00:16:58 -0400

    Add countdown timer

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,26 @@ import './assets/style.scss';
 import 'bulma/css/bulma.css';
 
 import { h, render } from 'preact';
+import { useState } from 'preact/hooks';
+import { useInterval } from './hooks/useInterval';
+
 
 function App() {
+	const [countdown, setCountdown] = useState(undefined);
+	const countdownDate = new Date('Aug 1, 2020 00:00:00').getTime();
+
+	useInterval(() => {
+		// https://www.w3schools.com/howto/howto_js_countdown.asp
+		const diff = countdownDate - new Date().getTime();
+
+		const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+		const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+		setCountdown(`${days} day, ${hours} hours, ${minutes} minutes, ${seconds} seconds`);
+	}, 1000);
+
 	return (
 		<div class='main'>
 			<section class='hero section is-dark'>
@@ -12,9 +30,15 @@ function App() {
 						The Tendos Podcast
 					</h2>
 
-					<h6 class="subtitle is-6 has-text-centered">
+					<h6 class='subtitle is-6 has-text-centered'>
 						A <b>Nintendo</b> podcast about <b>Nintendo</b>, buy <b>Nintendo</b>, for <b>Nintendo</b>.
 					</h6>
+				</div>
+
+				<div class='container'>
+					<h1 class='title is-1 has-text-centered has-text-danger'>
+						{countdown}
+					</h1>
 				</div>
 			</section>
 		</div>
