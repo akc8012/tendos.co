c49c2f25f7a3b2ed40537f7c23f6a4bfea4e8eff
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-29 23:19:12 -0400

    Add bulma and happy little CSS file

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
-// import './assets/style.scss';
+import './assets/style.scss';
+import 'bulma/css/bulma.css';
 
 import { h, render } from 'preact';
 
 function App() {
-	return <h1>heyo tendos!</h1>;
+	return (
+		<div class='main'>
+			<section class='hero section is-dark'>
+				<div class='container'>
+					<h2 class='title is-2 has-text-centered'>
+						The Tendos Podcast
+					</h2>
+
+					<h6 class="subtitle is-6 has-text-centered">
+						<i>A <b>Nintendo</b> podcast about <b>Nintendo</b>, buy <b>Nintendo</b>, for <b>Nintendo</b>.</i>
+					</h6>
+				</div>
+			</section>
+		</div>
+	);
 }
 
 render(<App />, document.body);
