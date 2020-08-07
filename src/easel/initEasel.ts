4d6ee2d02edc32c0ea654f9f9ca68c42306285d1
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-07 00:03:45 -0400

    Random methods, set random position

--- src/easel/initEasel.ts
+++ src/easel/initEasel.ts
@@ -18,8 +18,8 @@ export function initEasel() {
 function createFlake() {
 	flake = new createjs.Bitmap(flakeImage)
 
-	flake.x = 300;
-	flake.y = 300;
+	flake.x = randomArbitrary(0, stage.canvas.width);
+	flake.y = randomArbitrary(0, stage.canvas.height);
 
 	flake.scaleX = SCALE_FACTOR;
 	flake.scaleY = SCALE_FACTOR;
@@ -50,3 +50,24 @@ createjs.Ticker.addEventListener('tick', function () {
 
 	stage.update();
 });
+
+// https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function randomArbitrary(min: number, max: number) {
+	return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function randomInt(min: number, max: number) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
