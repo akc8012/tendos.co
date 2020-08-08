f73483be8c72c076de3a253e5f3b4205fd3db88d
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-07 23:54:27 -0400

    Added episode, got rid of timer, updated text

--- src/main.tsx
+++ src/main.tsx
@@ -3,26 +3,9 @@ import 'bulma/css/bulma.css';
 
 import { h, render } from 'preact';
 import { initEasel } from './easel/initEasel';
-import { useState } from 'preact/hooks';
-import { useInterval } from './hooks/useInterval';
 
 
 function App() {
-	const [countdown, setCountdown] = useState(undefined);
-	const countdownDate = new Date('Aug 8, 2020 00:00:00').getTime();
-
-	useInterval(() => {
-		// https://www.w3schools.com/howto/howto_js_countdown.asp
-		const diff = countdownDate - new Date().getTime();
-
-		const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-		const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-		const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-		setCountdown(`${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`);
-	}, 1000);
-
 	return (
 		<div class='main'>
 			<section class='hero section is-dark'>
@@ -40,13 +23,18 @@ function App() {
 					<h1 class='title is-1 has-text-weight-light'>
 						The show <b class='has-text-weight-normal'>will</b> go on.
 					</h1>
+
+					<audio controls>
+						<source src='https://tendostore.nyc3.cdn.digitaloceanspaces.com/tendos-episode101.mp3' type='audio/mpeg' />
+						Your browser does not support the audio element.
+					</audio>
+				</div>
+
+				<div class='container has-text-centered'>
 					<h4 class='subtitle is-4'>
 						<b class='has-text-danger'>neoTendos</b> has been uncancelled.
-						Tune in at midnight for a special episode with <b>andrew</b> and <b>joeT</b>.
+						Listen to our special episode with <b>andrew</b> and <b>joeT</b>.
 					</h4>
-					<h5 class='subtitle is-5 has-text-danger is-family-monospace has-text-weight-bold has-text-centered'>
-						{countdown}
-					</h5>
 				</div>
 			</section>
 		</div>
