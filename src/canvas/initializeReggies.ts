2ad115fed0b21988e95b6dff83724e6daa5f4223
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-13 18:46:38 -0400

    Introduce Reggie class

--- src/canvas/initializeReggies.ts
+++ src/canvas/initializeReggies.ts
@@ -1,44 +1,26 @@
 import * as createjs from 'createjs-module';
 import { createStage } from './createStage';
-import { random } from 'mathjs';
-
-import { createReggies } from './createReggies';
-import { updateReggies } from './updateReggies';
+import { IReggie, Reggie } from "./Reggie";
 
 
-export const REGGIE_COUNT = 8;
-export const SCALE_FACTOR = 0.2;
-
-export type Vector = { x: number, y: number };
-export type Size = { width: number, height: number };
+const REGGIE_COUNT = 8;
 
 export function initializeReggies() {
 	const stage = createStage();
 	const canvasSize = { width: stage.canvas.width, height: stage.canvas.height };
 
-	// TODO: Reggie class
-	const reggies = createReggies(canvasSize);
-	const velocities: Vector[] = [];
-
-	for (const reggie of reggies) {
-		stage.addChild(reggie);
-		velocities.push(getRandomVelocity());
+	const reggies: IReggie[] = [];
+	for (let i = 0; i < REGGIE_COUNT; i++) {
+		reggies.push(new Reggie(stage, canvasSize));
 	}
 
 	createjs.Ticker.framerate = 60;
 	createjs.Ticker.addEventListener('tick', function () {
-		updateReggies(reggies, velocities, canvasSize);
+		for (const reggie of reggies)
+			reggie.update(canvasSize);
+
 		stage.update();
 	});
 
 	console.log('my body is ready');
-}
-
-function getRandomVelocity(): Vector {
-	let result: Vector;
-
-	do result = { x: random(-2, 2), y: random(-2, 2) };
-	while (Math.abs(result.x) < 0.8 || Math.abs(result.y) < 0.8);
-
-	return result;
 }
