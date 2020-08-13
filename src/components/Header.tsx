edcc368ccb33e696dacf5a5e80b668898302c399
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-13 15:19:52 -0400

    Introduce Header and Subtitle components

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { h } from 'preact';
+
+
+export function Header() {
+	return (
+		<div class='container'>
+			<h2 class='title is-2 has-text-centered'>
+				The Tendos Podcast
+			</h2>
+
+			<h6 class='subtitle is-6 has-text-centered'>
+				A <b class='has-text-danger'>Nintendo</b> podcast about <b>Nintendo</b>, buy <b>Nintendo</b>, for <b>Nintendo</b>.
+			</h6>
+		</div>
+	);
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,15 @@ import 'bulma/css/bulma.css';
 
 import { h, render } from 'preact';
 import { EpisodePlayer } from './components/EpisodePlayer';
+import { Header } from './components/Header';
+import { Subtitle } from './components/Subtitle';
 
 
 function App() {
 	return (
 		<div class='main'>
 			<section class='hero section is-dark'>
-				<div class='container'>
-					<h2 class='title is-2 has-text-centered'>
-						The Tendos Podcast
-					</h2>
-
-					<h6 class='subtitle is-6 has-text-centered'>
-						A <b class='has-text-danger'>Nintendo</b> podcast about <b>Nintendo</b>, buy <b>Nintendo</b>, for <b>Nintendo</b>.
-					</h6>
-				</div>
+				<Header />
 
 				<div class='container has-text-centered'>
 					<h1 class='title is-1 has-text-weight-light'>
@@ -27,12 +21,7 @@ function App() {
 					<EpisodePlayer filename='tendos-episode101.mp3' />
 				</div>
 
-				<div class='container has-text-centered'>
-					<h4 class='subtitle is-4'>
-						<b class='has-text-danger'>neoTendos</b> has been uncancelled.
-						Listen to our special episode with <b>andrew</b> and <b>joeT</b>.
-					</h4>
-				</div>
+				<Subtitle />
 			</section>
 		</div>
 	);
