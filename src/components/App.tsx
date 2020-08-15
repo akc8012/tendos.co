4293d332e1b2a307df1273d0b5d1413578bcca58
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-14 23:00:11 -0400

    The latest episode is here

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,7 +13,7 @@ export function App() {
 
 				<div class='container has-text-centered'>
 					<EpisodeHeader />
-					<EpisodePlayer filename='tendos-episode101.mp3' />
+					<EpisodePlayer filename='tendos-episode102.mp3' />
 				</div>
 
 				<Subtitle />
--- src/components/Subtitle.tsx
+++ src/components/Subtitle.tsx
@@ -5,8 +5,7 @@ export function Subtitle() {
 	return (
 		<div class='container has-text-centered'>
 			<h4 class='subtitle is-4'>
-				<b class='has-text-danger'>neoTendos</b> has been uncancelled.
-				Listen to our special episode with <b>andrew</b> and <b>joeT</b>.
+				Listen to the latest. <b class='has-text-danger'>Tendos</b> is time. Time... for <b class='has-text-danger'>Tendos</b>.
 			</h4>
 		</div>
 	);
