b93522769beda0e5d7b2efd16ffcc4c09bb80f64
Andrew Colannino <akcolannino@email.neit.edu>
2020-09-05 11:13:40 -0400

    youtube button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -19,6 +19,14 @@ export function App() {
 					<EpisodePlayer filename='tendos-episode105.mp3' />
 				</div>
 
+				<div class='container'>
+					<form action='https://youtu.be/9O7siFWaSvs' method='get' target='_blank'>
+						<button class='button has-background-white-ter' type='submit'>
+							Listen on <b class='has-text-danger'>YouTube</b>
+						</button>
+					</form>
+				</div>
+
 				<Subtitle>
 					this is a <i>limited release</i> podcast, while supplies last
 				</Subtitle>
